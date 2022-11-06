79aec950e264d5f1a323d935694ce7278c006493
highshot-rk <sagarn62018@gmail.com>
2022-11-06 15:31:18 +0900

    base answer

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,19 @@
 
 class PagesController < ApplicationController
   def main
-    @customers = Customer.all
+    customers_generated = Customer.joins(:payment_requests).order('payment_requests.updated_at').where('payment_requests.status = ?', PaymentRequest.statuses[:generated]).select('first_name, last_name, status, amount, payment_requests.id as payment_id')
+    customers_responsed = Customer.joins(:payment_requests).where('payment_requests.status != ?', PaymentRequest.statuses[:generated]).select('first_name, last_name, status, amount, payment_requests.id as payment_id')
+
+    @customers = customers_generated - customers_responsed
+
+  end
+
+  def update_payment
+    if params[:payment_id].present?
+      PaymentRequest.update(params[:payment_id], status: PaymentRequest.statuses[:collected])
+      redirect_to :action => "main"
+    else
+      render :file => Rails.root.join('public', '404.html'), :status => 404
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@
 
 Rails.application.routes.draw do
   root "pages#main"
+  post '/update_payment', to: 'pages#update_payment'
 end
